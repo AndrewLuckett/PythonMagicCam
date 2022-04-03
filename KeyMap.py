915c091996cde1c49159faee118937bfd248bad8
Andrew Luckett <andyluckett9@gmail.com>
2022-04-03 02:56:52 +0100

    Add info to keymap file

--- KeyMap.py
+++ KeyMap.py
@@ -1,5 +1,10 @@
 from CamTypes import *
 
+# Use int values for keys if you run out of keyboard space
+# or dont want a mode bound to a key
+
+# Cant use ¬ or Esc as these keys are already bound
+
 keymap = {'w' : Full,
           'e' : Dvd,
           "r" : Random,
